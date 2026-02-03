3be88fd692131179c0c8dac09c9419a52698852c
Serhey A <madara_08@mail.ru>
2026-02-03 14:03:59 +0300

    feat(app): remove unnecessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.compiler)
     implementation(projects.core.data)
     implementation(projects.core.ui)
     implementation(projects.feature.discover)
