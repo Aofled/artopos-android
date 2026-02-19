7f3b41f50d53a41ddb12237887df0e154f645a79
Serhey A <madara_08@mail.ru>
2026-02-19 16:54:06 +0300

    feat(core-domain): update artwork repository and use cases with filtering

--- core/domain/src/main/kotlin/ru/createsmart/artopos/core/domain/repository/ArtworkRepository.kt
+++ core/domain/src/main/kotlin/ru/createsmart/artopos/core/domain/repository/ArtworkRepository.kt
@@ -3,7 +3,10 @@ package ru.createsmart.artopos.core.domain.repository
 import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import ru.createsmart.artopos.core.model.Artwork
+import ru.createsmart.artopos.core.model.FilterParams
 
 interface ArtworkRepository {
-    fun getPagedArtworks(): Flow<PagingData<Artwork>>
+    fun getPagedArtworks(
+        params: FilterParams,
+    ): Flow<PagingData<Artwork>>
 }
--- core/domain/src/main/kotlin/ru/createsmart/artopos/core/domain/usecase/GetArtworksUseCase.kt
+++ core/domain/src/main/kotlin/ru/createsmart/artopos/core/domain/usecase/GetArtworksUseCase.kt
@@ -4,12 +4,15 @@ import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import ru.createsmart.artopos.core.domain.repository.ArtworkRepository
 import ru.createsmart.artopos.core.model.Artwork
+import ru.createsmart.artopos.core.model.FilterParams
 import javax.inject.Inject
 
 class GetArtworksUseCase @Inject constructor(
     private val repository: ArtworkRepository,
 ) {
-    operator fun invoke(): Flow<PagingData<Artwork>> {
-        return repository.getPagedArtworks()
+    operator fun invoke(
+        params: FilterParams,
+    ): Flow<PagingData<Artwork>> {
+        return repository.getPagedArtworks(params)
     }
 }
