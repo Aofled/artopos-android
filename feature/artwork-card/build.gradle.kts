8b67b6310e94d9a0ab4b2eb88398d11ea31c770b
Serhey A <madara_08@mail.ru>
2026-03-25 23:37:08 +0300

    feat(feature-artwork-card): initialize module scaffolding

---
+++ feature/artwork-card/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("artopos.android.library")
+    id("artopos.android.library.compose")
+}
+
+android {
+    namespace = "ru.createsmart.artopos.feature.artworkcard"
+}
+
+dependencies {
+    implementation(projects.core.common)
+    implementation(projects.core.model)
+    implementation(projects.core.uiComponents)
+}
