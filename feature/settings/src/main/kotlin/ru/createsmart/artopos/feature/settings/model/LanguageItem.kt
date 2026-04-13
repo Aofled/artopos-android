6b9dec12858730eb32271aea785aacbf9a03e65e
Serhey A <madara_08@mail.ru>
2026-04-13 21:44:43 +0300

    feat(resources): add German language support

--- feature/settings/src/main/kotlin/ru/createsmart/artopos/feature/settings/model/LanguageItem.kt
+++ feature/settings/src/main/kotlin/ru/createsmart/artopos/feature/settings/model/LanguageItem.kt
@@ -17,5 +17,6 @@ object LanguageConfig {
         LanguageItem(tag = "be", nameResId = R.string.language_be),
         LanguageItem(tag = "ja", nameResId = R.string.language_ja),
         LanguageItem(tag = "zh", nameResId = R.string.language_zh),
+        LanguageItem(tag = "de", nameResId = R.string.language_de),
     )
 }
