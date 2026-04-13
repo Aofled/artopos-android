bdf59bd4a4fd85c1bdf9a731fbd1e8b8c44d0d2d
Serhey A <madara_08@mail.ru>
2026-04-13 22:22:16 +0300

    style(feature-settings): reorder language selection list

--- feature/settings/src/main/kotlin/ru/createsmart/artopos/feature/settings/model/LanguageItem.kt
+++ feature/settings/src/main/kotlin/ru/createsmart/artopos/feature/settings/model/LanguageItem.kt
@@ -12,13 +12,13 @@ object LanguageConfig {
     val supportedLanguages = listOf(
         LanguageItem(tag = "", nameResId = R.string.settings_language_option_system),
         LanguageItem(tag = "en", nameResId = R.string.language_en),
-        LanguageItem(tag = "ru", nameResId = R.string.language_ru),
-        LanguageItem(tag = "fr", nameResId = R.string.language_fr),
         LanguageItem(tag = "be", nameResId = R.string.language_be),
-        LanguageItem(tag = "ja", nameResId = R.string.language_ja),
-        LanguageItem(tag = "zh", nameResId = R.string.language_zh),
         LanguageItem(tag = "de", nameResId = R.string.language_de),
-        LanguageItem(tag = "it", nameResId = R.string.language_it),
         LanguageItem(tag = "es", nameResId = R.string.language_es),
+        LanguageItem(tag = "fr", nameResId = R.string.language_fr),
+        LanguageItem(tag = "it", nameResId = R.string.language_it),
+        LanguageItem(tag = "ja", nameResId = R.string.language_ja),
+        LanguageItem(tag = "ru", nameResId = R.string.language_ru),
+        LanguageItem(tag = "zh", nameResId = R.string.language_zh),
     )
 }
