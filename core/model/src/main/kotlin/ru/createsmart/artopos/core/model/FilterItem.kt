089e141835bf83b095dc664853c43656c785dc92
Serhey A <madara_08@mail.ru>
2026-04-18 15:58:04 +0300

    docs(core-models): add comments

--- core/model/src/main/kotlin/ru/createsmart/artopos/core/model/FilterItem.kt
+++ core/model/src/main/kotlin/ru/createsmart/artopos/core/model/FilterItem.kt
@@ -1,7 +1,7 @@
 package ru.createsmart.artopos.core.model
 
 data class FilterItem(
-    val uId: Long,
+    val uId: Long, // Unique ID for Compose/Room to prevent collisions between different FilterTypes
     val id: Long,
     val type: FilterType,
     val name: String,
